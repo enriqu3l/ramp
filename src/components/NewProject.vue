53a9f777b2540c4f2c0238e3ce9327a7b2f18a42
PabloAversa <pfaversa77@gmail.com>
2023-11-17 11:15:52 -0600

    create ImportPorject, NewProject, ProjectList components

---
+++ src/components/NewProject.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input type="text" v-model="color" />
+        <p> {{ color }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const color = ref("green");
+</script>
+
+<style scoped></style>
