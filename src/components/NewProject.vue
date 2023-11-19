08d06449cb16cacb77a6beaf6aa379f80dfcc87b
Enrique Lopez <enrique.lopez@Enriques-MacBook-Pro.local>
2023-11-19 17:49:32 -0600

    add new project component

--- src/components/NewProject.vue
+++ src/components/NewProject.vue
@@ -1,13 +1,52 @@
-<template>
-    <div>
-        <input type="text" v-model="color" />
-        <p> {{ color }}</p>
-    </div>
-</template>
+<script lang="ts">
+    import { appNames } from "../helpers/constants";
 
-<script setup>
-import { ref } from "vue";
-const color = ref("green");
+    export default{
+        data() {
+            return {
+                projectName: ""
+            }
+        },
+        methods: {
+            submit: function(e) {
+                e.preventDefault();
+                const projectExists = window.localStorage.getItem(this.projectName);
+
+                if (!projectExists) {
+                    window.localStorage.setItem(
+                        this.projectName,
+                        JSON.stringify({
+                        type: appNames.TEAM_HEALTH,
+                        votingSessions: [],
+                        actionItems: [],
+                        })
+                    );
+                    window.localStorage.setItem("currentProject", this.projectName);
+                    window.location.href = "dashboard";
+                } else {
+                    alert(
+                        `Project ${this.projectName} already exist, please choose a different name`
+                    );
+                }
+            }
+        }
+    };
 </script>
 
-<style scoped></style>
+<template>
+    <form @submit="submit">
+        <div class="input-group">
+            <input
+                v-model="projectName"
+                type="text"
+                placeholder="Project's name"
+                class="input input-bordered input-primary w-full max-w-xs"
+                name="projectName"
+                required
+            />
+            <button type="submit" id="newProjectSubmit" class="btn btn-primary"
+                >Create new Project
+            </button>
+        </div>
+    </form>
+</template>
---
+++ src/helpers/constants.ts
@@ -0,0 +1,11 @@
+const appNames = {
+    TEAM_HEALTH: "RAMP_TH",
+}
+
+const dataSets = {
+    teamHealth: {},
+    qaAssessment: {}
+}
+
+
+export { appNames, dataSets};
